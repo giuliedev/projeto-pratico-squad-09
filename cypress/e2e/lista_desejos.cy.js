e3d1017f9cb0a486a7b5a5a1220c5390eca04868
adeliadev <arodriguesthamyres@gmail.com>
2025-10-24 21:30:09 -0300

    Ajusta tempo de espera no teste da lista de desejos

--- cypress/e2e/lista_desejos.cy.js
+++ cypress/e2e/lista_desejos.cy.js
@@ -5,19 +5,27 @@ describe('template spec', () => {
 
     // fazer login de usuário
     cy.get('#user').type('joao@gmail.com')
+    cy.wait(600)
     cy.get('#password').type('senha12345')
+    cy.wait(600)
     cy.get('#btnLogin').click()
+    cy.wait(600)
     cy.get('body').click(400, 600)
 
     // ir para página de produto
     cy.get('.offcanvas-toggle.offside-menu').click()
+    cy.wait(600)
     cy.get('#menuShopText').click()
+    cy.wait(600)
     cy.get('body > div:nth-child(2) > div:nth-child(4) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(2) > ul:nth-child(2) > li:nth-child(1) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)').click()
+    cy.wait(600)
     cy.get("div[class='container'] div:nth-child(1) div:nth-child(1) div:nth-child(1) a:nth-child(1) img:nth-child(2)").click()
 
     // adicionar produto na lista de desejos
     cy.get('div[class="links_Product_areas"] a[title="Wishlist"]').click()
+    cy.wait(600)
     cy.get('body').click(400, 600)
+    cy.wait(600)
 
     // verificar lista de desejos
     cy.get('div[class="mobile-right-side"] i[class="fa fa-heart"]').click()
